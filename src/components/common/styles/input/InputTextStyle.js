9b8e0f24bc6b268351c68d1d3996e9dd8c2e4495
anand9773 <aaanand9773@gmail.com>
2023-12-25 14:04:58 +0530

    loading screen done

---
+++ src/components/common/styles/input/InputTextStyle.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+const inputStyle = StyleSheet.create({
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 16,
+    padding: 8,
+    width: "100%",
+    borderRadius: 8, // Add rounded corners
+  },
+});
+export default inputStyle;
